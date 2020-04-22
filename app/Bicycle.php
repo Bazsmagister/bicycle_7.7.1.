c35dd64b2d5f9e26587792af771eec72bb8a603b
Bazsmagister <bosmer007@gmail.com>
2020-04-22 17:35:30 +0200

    stuck

--- app/Bicycle.php
+++ app/Bicycle.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bicycle extends Model
 {
-    //
+
     //public $timestamps = false;
 
     protected $guarded = [];
+
+    //protected $fillable = [''];
 }
