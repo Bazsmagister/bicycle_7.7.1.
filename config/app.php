9cae07b3b7cfd8313890a531ba7d0d15b41b6e9a
Bazsmagister <bosmer007@gmail.com>
2020-06-17 09:10:44 +0200

    custom helper readme2

--- config/app.php
+++ config/app.php
@@ -163,8 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
-        App\Providers\HelperServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -178,6 +177,9 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Spatie\Permission\PermissionServiceProvider::class,
+        App\Providers\HelperServiceProvider::class,
 
     ],
 
